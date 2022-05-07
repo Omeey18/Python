57e6dc9febfb76ed0e12e117aa4e251997a524b0
Omeey18 <Om.Patel@einfochips.com>
2022-05-07 18:20:15 +0530

    Adding work

---
+++ Day1_07_05_22/NameGanerator.py
@@ -0,0 +1,8 @@
+
+print("-------Welcome to Name Ganerator--------\n")
+CityName = input("Entewr your city name: ")
+PetName = input("Enter your Pet name: ")
+
+print("----------------------------------")
+print("My suggestion: " + CityName + PetName)
+print("----------------------------------\n")
